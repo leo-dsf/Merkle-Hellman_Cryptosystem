983501c2a168c978828cea92fec99d9b2345ac54
leo-dsf <leonardodsf2002@gmail.com>
2021-12-22 17:22:53 +0000

    Formatação e Mudança no main
    - Formatação do código.
    - Mudança no main para meter as funções a correr ao mesmo tempo e escrever os dados para um ficheiro de texto.

--- subset_sum_problem.c
+++ subset_sum_problem.c
@@ -7,6 +7,8 @@
 //
 // Gabriel Hall Abreu, 102851
 //
+// Diogo Alves da Silva, 103925
+//
 
 #if __STDC_VERSION__ < 199901L
 #error "This code must must be compiled in c99 mode or later (-std=c99)" // to handle the unsigned long long data type
@@ -31,7 +33,7 @@
 // First approach to the problem
 //
 
-char bruteForceV1(int n, integer_t p[n], integer_t desired_sum)
+char bruteForceV1(int n, const integer_t p[n], integer_t desired_sum)
 {
     for (int comb = 0; comb < (1 << n); ++comb) // (1 << n) == pow(2,n)
     {
@@ -53,7 +55,7 @@ char bruteForceV1(int n, integer_t p[n], integer_t desired_sum)
     return 0;
 }
 
-char bruteForceV2(int n, integer_t p[n], integer_t desired_sum, int result[])
+char bruteForceV2(int n, const integer_t p[n], integer_t desired_sum, int result[])
 {
     for (int comb = 0; comb < (1 << n); ++comb)
     {
@@ -79,9 +81,11 @@ char bruteForceV2(int n, integer_t p[n], integer_t desired_sum, int result[])
     }
     return 0;
 }
+
 //
 // Second approach to the problem
 //
+
 char bruteForceRecursiveV1(int n, integer_t p[n], integer_t desired_sum, int current_index, integer_t partial_sum)
 {
     if (partial_sum == desired_sum)
@@ -137,9 +141,14 @@ char bruteForceRecursiveV3(int n, integer_t p[n], integer_t desired_sum, int cur
     return 0;
 }
 
+//
+// Third approach to the problem
+//
+//
 // HorowitzSahni starts here
-
-// Implementig quick sort function
+//
+// Implementing quick sort function
+//
 
 void swap(integer_t *a, integer_t *b)
 {
@@ -153,7 +162,8 @@ the pivot element at its correct position in sorted
 array, and places all smaller (smaller than pivot)
 to left of pivot and all greater elements to right
 of pivot */
-int partition(integer_t arr[], integer_t low, integer_t high)
+
+integer_t partition(integer_t arr[], integer_t low, integer_t high)
 {
     integer_t pivot = arr[high]; // pivot
     integer_t i = (low - 1);     // Index of smaller element and indicates the right position of pivot found so far
@@ -175,6 +185,7 @@ int partition(integer_t arr[], integer_t low, integer_t high)
 arr[] --> Array to be sorted,
 low --> Starting index,
 high --> Ending index */
+
 void quickSort(integer_t arr[], integer_t low, integer_t high)
 {
     if (low < high)
@@ -190,30 +201,32 @@ void quickSort(integer_t arr[], integer_t low, integer_t high)
     }
 }
 
-// Funciton to transform decimal into binary
+//
+// Function to transform decimal into binary
 // n = number | i = binary length -> it will return in reverse order
-char decToBinary(integer_t n, integer_t c)
-{
+//
 
-    for (integer_t i = 0; i < c; i++)
+char decToBinary(integer_t n, integer_t c, int result[], int index)
+{
+    for (integer_t i = index; i < c + index; i++)
     {
-        integer_t k = n >> i;
+        integer_t k = n >> (i - index);
         if (k & 1)
         {
-            printf("%s", "1");
+            result[i] = 1;
         }
         else
         {
-            printf("%s", "0");
+            result[i] = 0;
         }
     }
     return 0;
 }
 
-char HorowitzSahni(int n, integer_t p[n], integer_t desired_sum)
+char HorowitzSahni(int n, const integer_t p[n], integer_t desired_sum, int result[])
 {
-    integer_t lengthP1 = n % 2 + n / 2;
-    integer_t lengthP2 = n / 2;
+    int lengthP1 = n % 2 + n / 2;
+    int lengthP2 = n / 2;
     integer_t p1[n % 2 + n / 2];
     integer_t p2[n / 2];
 
@@ -240,7 +253,7 @@ char HorowitzSahni(int n, integer_t p[n], integer_t desired_sum)
     duplicated_s1 = (integer_t *)malloc(total1 * sizeof(integer_t));
     duplicated_s2 = (integer_t *)malloc(total2 * sizeof(integer_t));
 
-    // Calculating subset sums and duplicating arrays -> Need to find an other way to save sums indexes
+    // Calculating subset sums and duplicating arrays -> Need to find another way to save sums indexes
     // When converting the index to binary and inverting order I will have the result
 
     integer_t i = 0;
@@ -278,31 +291,30 @@ char HorowitzSahni(int n, integer_t p[n], integer_t desired_sum)
     integer_t j = total2 - 1;
     while (k < total1 && j >= 0)
     {
-
         if (s1[k] + s2[j] == desired_sum)
         {
             // finding indexes
-            integer_t c = 0;
-            while (c < total1)
+            integer_t count = 0;
+            while (count < total1)
             {
-                if (duplicated_s1[c] == s1[k])
+                if (duplicated_s1[count] == s1[k])
                 {
-                    decToBinary(c, lengthP1);
+                    decToBinary(count, lengthP1, result, 0);
                     break;
                 }
-                c++;
+                count++;
             }
-            c = 0;
-            while (c < total2)
+            count = 0;
+            while (count < total2)
             {
-                if (duplicated_s2[c] == s2[j])
+                if (duplicated_s2[count] == s2[j])
                 {
-                    decToBinary(c, lengthP2);
+                    decToBinary(count, lengthP2, result,lengthP1);
                     break;
                 }
-                c++;
+                count++;
             }
-            break;
+            return 1;
         }
         else if (s1[k] + s2[j] < desired_sum)
         {
@@ -320,225 +332,10 @@ char HorowitzSahni(int n, integer_t p[n], integer_t desired_sum)
     return 0;
 }
 
-
-
-//QUARTA E ULTIMA FUNÇÃO
-//HEAPS
-//NODE IS AT INDEX I
-//LEFT CHILD IS AT 2*I
-//RIGHT CHILD IS AT 2*I+1
-//ITS PARENT IS AT 2/I
-//BINARY TREE HAS 2^(H-1)-1 NODES, H = HEIGHT OF BINARY TREE
-//minuto 31:54 https://www.youtube.com/watch?v=HqPJF2L5h9U
-//https://gist.github.com/sudhanshuptl/d86da25da46aa3d060e7be876bbdb343 -> min heap implementation
-//https://www.journaldev.com/36805/min-heap-binary-tree -> another min heap implementation
-
-typedef struct MinHeap MinHeap;
-struct MinHeap {
-    int* arr;
-    // Current Size of the Heap
-    int size;
-    // Maximum capacity of the heap
-    int capacity;
-};
- 
-int parent(int i) {
-    // Get the index of the parent
-    return (i - 1) / 2;
-}
- 
-int left_child(int i) {
-    return (2*i + 1);
-}
- 
-int right_child(int i) {
-    return (2*i + 2);
-}
- 
-int get_min(MinHeap* heap) {
-    // Return the root node element,
-    // since that's the minimum
-    return heap->arr[0];
-}
- 
-MinHeap* init_minheap(int capacity) {
-    MinHeap* minheap = (MinHeap*) calloc (1, sizeof(MinHeap));
-    minheap->arr = (int*) calloc (capacity, sizeof(int));
-    minheap->capacity = capacity;
-    minheap->size = 0;
-    return minheap;
-}
- 
-MinHeap* insert_minheap(MinHeap* heap, int element) {
-    // Inserts an element to the min heap
-    // We first add it to the bottom (last level)
-    // of the tree, and keep swapping with it's parent
-    // if it is lesser than it. We keep doing that until
-    // we reach the root node. So, we will have inserted the
-    // element in it's proper position to preserve the min heap property
-    if (heap->size == heap->capacity) {
-        fprintf(stderr, "Cannot insert %d. Heap is already full!\n", element);
-        return heap;
-    }
-    // We can add it. Increase the size and add it to the end
-    heap->size++;
-    heap->arr[heap->size - 1] = element;
- 
-    // Keep swapping until we reach the root
-    int curr = heap->size - 1;
-    // As long as you aren't in the root node, and while the 
-    // parent of the last element is greater than it
-    while (curr > 0 && heap->arr[parent(curr)] > heap->arr[curr]) {
-        // Swap
-        int temp = heap->arr[parent(curr)];
-        heap->arr[parent(curr)] = heap->arr[curr];
-        heap->arr[curr] = temp;
-        // Update the current index of element
-        curr = parent(curr);
-    }
-    return heap; 
-}
- 
-MinHeap* heapify(MinHeap* heap, int index) {
-    // Rearranges the heap as to maintain
-    // the min-heap property
-    if (heap->size <= 1)
-        return heap;
-     
-    int left = left_child(index); 
-    int right = right_child(index); 
- 
-    // Variable to get the smallest element of the subtree
-    // of an element an index
-    int smallest = index; 
-     
-    // If the left child is smaller than this element, it is
-    // the smallest
-    if (left < heap->size && heap->arr[left] < heap->arr[index]) 
-        smallest = left; 
-     
-    // Similarly for the right, but we are updating the smallest element
-    // so that it will definitely give the least element of the subtree
-    if (right < heap->size && heap->arr[right] < heap->arr[smallest]) 
-        smallest = right; 
- 
-    // Now if the current element is not the smallest,
-    // swap with the current element. The min heap property
-    // is now satisfied for this subtree. We now need to
-    // recursively keep doing this until we reach the root node,
-    // the point at which there will be no change!
-    if (smallest != index) 
-    { 
-        int temp = heap->arr[index];
-        heap->arr[index] = heap->arr[smallest];
-        heap->arr[smallest] = temp;
-        heap = heapify(heap, smallest); 
-    }
- 
-    return heap;
-}
- 
-MinHeap* delete_minimum(MinHeap* heap) {
-    // Deletes the minimum element, at the root
-    if (!heap || heap->size == 0)
-        return heap;
- 
-    int size = heap->size;
-    int last_element = heap->arr[size-1];
-     
-    // Update root value with the last element
-    heap->arr[0] = last_element;
- 
-    // Now remove the last element, by decreasing the size
-    heap->size--;
-    size--;
- 
-    // We need to call heapify(), to maintain the min-heap
-    // property
-    heap = heapify(heap, 0);
-    return heap;
-}
- 
-MinHeap* delete_element(MinHeap* heap, int index) {
-    // Deletes an element, indexed by index
-    // Ensure that it's lesser than the current root
-    heap->arr[index] = get_min(heap) - 1;
-     
-    // Now keep swapping, until we update the tree
-    int curr = index;
-    while (curr > 0 && heap->arr[parent(curr)] > heap->arr[curr]) {
-        int temp = heap->arr[parent(curr)];
-        heap->arr[parent(curr)] = heap->arr[curr];
-        heap->arr[curr] = temp;
-        curr = parent(curr);
-    }
- 
-    // Now simply delete the minimum element
-    heap = delete_minimum(heap);
-    return heap;
-}
- 
-void print_heap(MinHeap* heap) {
-    // Simply print the array. This is an
-    // inorder traversal of the tree
-    printf("Min Heap:\n");
-    for (int i=0; i<heap->size; i++) {
-        printf("%d -> ", heap->arr[i]);
-    }
-    printf("\n");
-}
- 
-void free_minheap(MinHeap* heap) {
-    if (!heap)
-        return;
-    free(heap->arr);
-    free(heap);
-}
-/*
-    //Capacity of 10 elements
-    MinHeap* heap = init_minheap(10);
- 
-    insert_minheap(heap, 40);
-    insert_minheap(heap, 50);
-    insert_minheap(heap, 5);
-    print_heap(heap);
-     
-    // Delete the heap->arr[1] (50)
-    delete_element(heap, 1);
- 
-    print_heap(heap);
-    free_minheap(heap);
-    return 0;
-*/
-
-
-
-char SchroeppelShamir(int n, integer_t p[n], integer_t desired_sum)
-{
-    //Dividir p em 4 partes nearly iguais
-    integer_t lengthP1 = n % 2 + n / 2;
-    integer_t lengthP2 = n / 2;
-    integer_t lp1 = lengthP1 % 2 + lengthP1 / 2;
-    integer_t lp2 = lengthP1 / 2;
-    integer_t lp3 = lengthP2 % 2 + lengthP2 / 2;
-    integer_t lp4 = lengthP2 / 2;
-    
-    integer_t p1[lp1];
-    integer_t p2[lp2];
-    integer_t p3[lp3];
-    integer_t p4[lp4];
-
-    //Fazer arrays a1 a2 b1 b2 em que são geradas como min e max heaps sem usar memoria???????????????????????
-
-
-
-    //
-}
-
-
 //
 // main program
 //
+
 int main(void)
 {
     /*
@@ -550,46 +347,64 @@ int main(void)
     fprintf(stderr,"  integer_t ... %d bits\n",8 * (int)sizeof(integer_t));
      */
 
+    // FILE *fp = fopen("data.txt","w");
+
     for (int i = 22; i < n_problems; i++)
     {
         int n = all_subset_sum_problems[i].n; // The value of n
-
-        if (n > 57)
-            continue; // Skip large values of n
 
         integer_t *p = all_subset_sum_problems[i].p; // The weights
         for (int k = 0; k < n_sums; k++)
         {
             integer_t desired_sum = all_subset_sum_problems[i].sums[k]; // The desire_sum
-            // Array with the result
-            int result[n];
-
+            int result[n]; // Array with the result
             for (int j = 0; j < n; j++)
                 result[j] = 0;
-            printf("n = %d \n", n);
-            double t1 = cpu_time();
-            HorowitzSahni(n, p, desired_sum);
-            printf("\n");
-            double t2 = cpu_time();
-            printf("elapsed time: %.6f seconds\n", t2 - t1);
+            char found;
 
-            // printf("N = %d, Found: %d, ", n, bruteForceV2(n, p, desired_sum, result));
-            // printf("%d\n", bruteForceV1(n, p, desired_sum));
-            // printf("N = %d, Found: %d, ", n, bruteForceRecursiveV1(n, p, desired_sum, 0,0));
-            // printf("N = %d, Found: %d, ", n, bruteForceRecursiveV2(n, p, desired_sum, 0,0,result));
-            // double t1 = cpu_time();
-            // printf("Para n = %d | Found: %d | ", n, bruteForceRecursiveV3(n, p, desired_sum, 0, 0, result));
-
-            // printf("Result: ");
-
-            // for (int j = 0; j < n; j++)
-            //     printf("%d", result[j]);
             //
-            // printf("\n");
-            //  double t2 = cpu_time();
-            //  printf("elapsed time: %.6f seconds\n", t2 - t1);
+            // bruteForce() ---> (id = 0)
+            //
+
+            /*
+            if (n >= 30)
+            {
+                double t1 = cpu_time();
+                found = bruteForceV2(n, p, desired_sum, result);
+                double t2 = cpu_time();
+                fprintf(fp, "%d %d %d %d %f\n", 0, n, k, found, t2 - t1);
+            }
+
+            //
+            // bruteForceRecursive() ---> (id = 1)
+            //
+
+            if (n >= 40)
+            {
+                double t1 = cpu_time();
+                found = bruteForceRecursiveV3(n, p, desired_sum, 0, 0, result);
+                double t2 = cpu_time();
+                fprintf(fp, "%d %d %d %d %f\n", 1, n, k, found, t2 - t1);
+            }
+            */
+
+            //
+            // HorowitzSahni() ---> (id = 2)
+            //
+
+            double t1 = cpu_time();
+            found = HorowitzSahni(n, p, desired_sum, result);
+            double t2 = cpu_time();
+            // fprintf(fp, "%d %d %d %d %f\n", 2, n, k, found, t2 - t1);
+
+            printf("For n = %d | Found: %d | Time: %f (s) | ", n, found, t2-t1);
+            printf("Result: ");
+            for (int j = 0; j < n; j++)
+                 printf("%d", result[j]);
+             printf("\n");
             break;
         }
     }
+    // fclose(fp);
     return 0;
 }
